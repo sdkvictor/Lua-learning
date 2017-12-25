57fd3dcbcb5849e9d407aaf2230f069934f3fe40
Victor Villarreal <a01039863@itesm.mx>
2017-12-24 21:50:31 -0600

    Notes 24/12/2017
    notes

---
+++ Code notes.lua
@@ -0,0 +1,31 @@
+print(--can be either variable or text)
+
+if condition then --no need for parenthesis
+end
+
+if condition then
+	--action
+elseif other_condition then
+	--action
+else
+	--action
+end
+
+while condition do
+end
+
+while not condition do
+end
+
+--comment
+
+and -- works as && from c++
+
+boolean = true --no need to declare
+boolean == false 
+
+ctrl + c ---> end the program
+
+inventory = {"a", "b", "c", 1, {}}
+
+
